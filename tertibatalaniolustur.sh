0b59a8601ecabd8ee0c7b5baaeefc24700d18e3b
cevdetabbas <92764212+cevdetabbas@users.noreply.github.com>
2022-05-18 10:56:04 -0500

    Update tertibatalaniolustur.sh

--- tertibatalaniolustur.sh
+++ tertibatalaniolustur.sh
@@ -2,14 +2,14 @@
 yum update -y
 useradd tm
 echo -e .\n.\n|sudo passwd tm
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/ipassignet
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/trusttanal
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/trustayolla
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/trustabaglan
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/risktenal
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/riskeyolla
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/risebaglan
-wget https://github.com/cevdetabbas/scriptFiles/blob/main/ipassignet
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/main/ipassignet
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/trusttanal
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/trustayolla
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/trustabaglan
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/risktenal
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/riskeyolla
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/risebaglan
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/blob/main/ipassignet
 chmod 777 trust*
 chmod 777 risk*
 chmod 777 ipassignet
