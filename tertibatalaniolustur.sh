b372bc51425ec28d74e27994b12fa9742490856f
cevdetabbas <92764212+cevdetabbas@users.noreply.github.com>
2022-05-18 14:00:49 -0500

    Update tertibatalaniolustur.sh

--- tertibatalaniolustur.sh
+++ tertibatalaniolustur.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum update -y
+yum install -y bash-completion
+yum install -y epel-release
 useradd tm
 echo -e .\n.\n|sudo passwd tm
 wget https://raw.githubusercontent.com/cevdetabbas/Enaktarlar/main/riskEnaktar.key
@@ -22,3 +24,34 @@ mv trust* /bin -f
 mv risk* /bin -f
 mv ipassignet /bin -f
 ipassignet $1 $2
+
+### simdi bundan sonrasi extra yuklemeleri hazirlayan scriptler yukle...lar olacak
+
+# yukle nomachine
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/main/yuklenomachine
+chmod 777 yuklenomachine
+mv yuklenomachine /bin
+
+# yukle chrome
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/main/yuklechrome
+chmod 777 yuklechrome
+mv yuklechrome /bin
+
+# yukle vnc
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/main/yuklevnc
+chmod 777 yuklevnc
+mv yuklevnc /bin
+
+# yukle snapd
+wget https://raw.githubusercontent.com/cevdetabbas/scriptFiles/main/yuklesnapd
+chmod 777 yuklesnapd
+mv yuklesnapd /bin
+
+# yukle idea toolbox
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.24.11947.tar.gz
+tar xvf jetbrains-toolbox-1.24.11947.tar.gz
+./jetbrains-toolbox-1.24.11947.tar.gz
+
+#
+
+
