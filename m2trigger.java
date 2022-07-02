10a4e4900c559f0ed1c0562426b80569f8280d58
cevdetabbas <cevdetabbas@gmail.com>
2022-07-02 15:22:32 -0500

    snap

---
+++ m2trigger.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class m2trigger
+{
+    public static void main(String[] args) throws IOException
+    {
+        Process process = Runtime.getRuntime().exec("m2.sh");
+
+    }
+}
