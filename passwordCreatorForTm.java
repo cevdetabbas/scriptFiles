45db1d381b958ee221da0e218599bc1bb932e785
cevdetabbas <cevdetabbas@gmail.com>
2022-07-02 22:29:26 -0500

    snap

--- passwordCreatorForTm.java
+++ passwordCreatorForTm.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -5,11 +8,24 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class passwordCreatorForTm
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd_HH:mm");
         Date date = new Date();
-        System.out.println("toor_"+formatter.format(date));
+        String rpw = "toor_"+formatter.format(date);
+        System.out.println(rpw);
+        BufferedWriter writer = new BufferedWriter( new FileWriter("rpw.txt"));
+        writer.write(rpw);
+        writer.close();
+        String rpwCommand = "sudo su -l -c 'printf \""+rpw+"\\n"+rpw+"\\n\\n\" | passwd root'\n";
+        System.out.println(rpwCommand);
+        BufferedWriter writer2 = new BufferedWriter( new FileWriter("rpwCommand.txt"));
+        writer2.write(rpwCommand);
+        writer2.close();
+
+
+
+
     }
 
 }
